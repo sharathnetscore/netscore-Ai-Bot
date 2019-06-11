2a9c9d44235685d28428712fbb1159075ccd509b
sharathnetscore <51479949+sharathnetscore@users.noreply.github.com>
2019-06-11 01:26:05 -0700

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ $method = $_SERVER['REQUEST_METHOD'];
 if($method == 'POST')
 {
 	$requestBody = file_get_contents('php://input');
+	echo $requestBody;
 	$json = json_decode($requestBody);
 
 	$text = $json->result->parameters->text;
